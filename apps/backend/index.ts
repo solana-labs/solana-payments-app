f3dbeecfc8e371c740fe77f7dc42e9ff690f63a4
littlejohntj <tjl13c@my.fsu.edu>
2023-02-20 15:22:17 -0700

    setting up hot reloading, i have most of it i think

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,17 +1,23 @@
 import express from 'express'
+import ngrok from 'ngrok'
 
 const app = express()
 
-const port = 8000
+const port = 3001
 
 app.get('/', (req, res) => {
-    res.send('Hello World My Dude')
+    res.send('Hello World My Sis')
 })
 
 app.get('/hi', (req, res) => {
-    res.send('Bye My Dude')
+    res.send('Bye My Man')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, async () => {
+    const ngrokUrl = process.env.NGROK_URL
+    const url = await ngrok.connect(port)
+    console.log(url)
+
+    // console.log(`Example app listening at http://localhost:${port}`)
+    // console.log(`Server is accessible at ${url}`)
 })
