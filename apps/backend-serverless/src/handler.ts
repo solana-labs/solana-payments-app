83d8231e42fb2a89fa38693cc6ffaee31a0f9f07
littlejohntj <tjl13c@my.fsu.edu>
2023-04-29 00:58:10 -0400

    going to merge back into aws migration branch

--- apps/backend-serverless/src/handler.ts
+++ apps/backend-serverless/src/handler.ts
@@ -6,13 +6,11 @@ const prisma = new PrismaClient()
 export const hello = async (
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-    // const merchants = await prisma.merchant.findMany()
-
     return {
         statusCode: 200,
         body: JSON.stringify(
             {
-                merchants: 'hello serverless!',
+                merchants: 'rip...',
             },
             null,
             2
