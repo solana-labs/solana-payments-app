9d17030dd63f5cf62c686635c8c57e6a785effc0
littlejohntj <tjl13c@my.fsu.edu>
2023-05-01 17:20:11 -0400

    got some validation

--- apps/backend-serverless/src/handlers/payment.ts
+++ apps/backend-serverless/src/handlers/payment.ts
@@ -1,31 +1,55 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import {
+    ShopifyPaymentInitiation,
+    parseAndValidateShopifyPaymentInitiation,
+} from '../models/process-payment-request.model.js'
+import { requestErrorResponse } from '../utilities/request-response.utility.js'
 
 export const payment = async (
-  event: APIGatewayProxyEvent
+    event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const paymentUiUrl = process.env.PAYMENT_UI_URL;
+    // the first thing I want to do is parse the body of the request
+    // then i can take that info and determine if i need to
+    // 1. create a new PaymentRecord
+    // 2. reuse an existing PaymentRecord
 
-  if (paymentUiUrl == null) {
+    if (event.body == null) {
+        return requestErrorResponse(new Error('Missing body.'))
+    }
+
+    let paymentInitiation: ShopifyPaymentInitiation
+
+    try {
+        paymentInitiation = parseAndValidateShopifyPaymentInitiation(
+            JSON.parse(event.body)
+        )
+    } catch (error) {
+        return requestErrorResponse(error)
+    }
+
+    const paymentUiUrl = process.env.PAYMENT_UI_URL
+
+    if (paymentUiUrl == null) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(
+                {
+                    message: 'Missing information.',
+                },
+                null,
+                2
+            ),
+        }
+    }
+
     return {
-      statusCode: 500,
-      body: JSON.stringify(
-        {
-          message: "Missing information.",
-        },
-        null,
-        2
-      ),
-    };
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        redirect_url: paymentUiUrl,
-      },
-      null,
-      2
-    ),
-  };
-};
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                redirect_url: paymentUiUrl,
+            },
+            null,
+            2
+        ),
+    }
+}
--- apps/backend-serverless/src/models/process-payment-request.model.ts
+++ apps/backend-serverless/src/models/process-payment-request.model.ts
@@ -1,54 +1,74 @@
-import { object, string, InferType } from "yup";
+import { object, string, number, InferType, boolean } from 'yup'
 
 /*
-    SPEC: https://shopify.dev/docs/apps/payments/implementation/process-a-payment/offsite#client_details-hash
+    SPEC: https://shopify.dev/docs/apps/payments/implementation/process-a-payment/offsite
 */
 
 const customerAddressSchema = object().shape({
-  given_name: string().optional(),
-  family_name: string().required(),
-  line1: string().required(),
-  line2: string().optional(),
-  city: string().required(),
-  postal_code: string().required(),
-  province: string().required(),
-  country: string().required(),
-  phone_number: string().optional(),
-  company: string().required(),
-});
+    given_name: string().optional(),
+    family_name: string().required(),
+    line1: string().required(),
+    line2: string().optional(),
+    city: string().required(),
+    postal_code: string().optional(),
+    province: string().optional(),
+    country: string().required(),
+    phone_number: string().optional(),
+    company: string().required(),
+})
 
 const shopifyPaymentInitiationCustomerScheme = object().shape({
-  email: string().required(),
-  phone_number: string().required(),
-  locale: string().required(),
-  billing_address: customerAddressSchema.required(),
-  shipping_address: customerAddressSchema.required(),
-});
+    email: string().optional(),
+    phone_number: string().optional(),
+    locale: string().required(),
+    billing_address: customerAddressSchema.required(),
+    shipping_address: customerAddressSchema.required(),
+})
 
 const paymentMethodDataSchema = object().shape({
-  cancel_url: string().required(),
-});
+    cancel_url: string().required(),
+})
 
 const paymentMethodSchema = object().shape({
-  type: string().required(),
-  data: paymentMethodDataSchema.required(),
-});
+    type: string().required(),
+    data: paymentMethodDataSchema.required(),
+})
 
 export const shopifyPaymentInitiationScheme = object().shape({
-  id: string().required(),
-  gid: string().required(),
-  group: string().required(),
-  amount: string().required(), // must be numeric
-  currency: string().required(), // three string IOS 4217 code
-  test: string().required(),
-  merchant_locale: string().required(),
-  payment_method: paymentMethodSchema.required(),
-  proposed_at: string().required(),
-  kind: string().required(),
-  customer: shopifyPaymentInitiationCustomerScheme.optional(),
-  client_details: string().optional(),
-});
+    id: string().required(),
+    gid: string().required(),
+    group: string().required(),
+    amount: number().required(), // must be numeric
+    currency: string().required(), // three string IOS 4217 code
+    test: boolean().required(),
+    merchant_locale: string().required(),
+    payment_method: paymentMethodSchema.required(),
+    proposed_at: string().required(),
+    kind: string().required(),
+    customer: shopifyPaymentInitiationCustomerScheme.optional(),
+})
 
-export type ShopifyPaymentInitiationScheme = InferType<
-  typeof shopifyPaymentInitiationScheme
->;
+export type ShopifyPaymentInitiation = InferType<
+    typeof shopifyPaymentInitiationScheme
+>
+
+export const parseAndValidateShopifyPaymentInitiation = (
+    paymentInitiationRequestBody: any
+): ShopifyPaymentInitiation => {
+    let parsedPaymentInitiationRequestBody: ShopifyPaymentInitiation
+    try {
+        parsedPaymentInitiationRequestBody =
+            shopifyPaymentInitiationScheme.cast(
+                paymentInitiationRequestBody
+            ) as ShopifyPaymentInitiation
+    } catch (error) {
+        if (error instanceof Error) {
+            throw error
+        } else {
+            throw new Error(
+                'Could not parse the payment initiation request body. Unknown Reason.'
+            )
+        }
+    }
+    return parsedPaymentInitiationRequestBody
+}
