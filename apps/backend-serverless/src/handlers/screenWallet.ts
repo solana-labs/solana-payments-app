ef3e325fa2072bb9df818018c3f232337e28c67c
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-05-03 15:43:04 -0400

    Added models, and response validation

--- apps/backend-serverless/src/handlers/screenWallet.ts
+++ apps/backend-serverless/src/handlers/screenWallet.ts
@@ -1,8 +1,10 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import { screenAddress } from '../services/trm.server.js';
+import { screenAddress, validateTrmResponse } from '../services/trm.service.js';
 
 export const screenWallet: APIGatewayProxyHandler = async (event) => {
-  const address = event.queryStringParameters?.address;
+  //   const address = event.queryStringParameters?.address;
+  const address = 'Htp9MGP8Tig923ZFY7Qf2zzbMUmYneFRAhSp7vSg4wxV';
+  //   const address = '0xdac17f958d2ee523a2206206994597c13d831ec7';
 
   if (!address) {
     return {
@@ -14,10 +16,20 @@ export const screenWallet: APIGatewayProxyHandler = async (event) => {
   try {
     const response = await screenAddress(address);
 
+    const riskLevelBelow5 = response.every((item: any) =>
+      item.entities.every((entity: any) => entity.riskScoreLevel < 5)
+    );
+
     return {
       statusCode: 200,
-      body: JSON.stringify(response.data),
+      body: JSON.stringify({ riskLevelBelow5 }),
     };
+
+    // console.log('response data', response.data);
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify(screenedAddress),
+    // };
   } catch (error: any) {
     return {
       statusCode: error.response.status,
