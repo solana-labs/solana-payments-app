40951bee9646bfbfccfb5605e8da65567a96d73c
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-05-03 22:42:13 -0400

    Revert "Transaction monitoring"

--- apps/backend-serverless/src/configs/trm.config.ts
+++
@@ -1,4 +0,0 @@
-export const TRM_API_KEY = process.env.TRM_API_KEY;
-export const TRM_CHAIN_SOLANA_ID = 'solana';
-export const TRM_SCREEN_URL =
-  'https://api.trmlabs.com/public/v2/screening/addresses';
