60b546b5039676d998af32e82ff49dbf7d1d955d
littlejohntj <tjl13c@my.fsu.edu>
2023-05-08 11:46:37 -0400

    rip 4 later

--- apps/backend-serverless/src/handlers/redirect.ts
+++ apps/backend-serverless/src/handlers/redirect.ts
@@ -1,67 +1,67 @@
-import { PrismaClient } from "@prisma/client";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { AppRedirectQueryParam } from "../models/redirect-query-params.model.js";
-import { fetchAccessToken } from "../services/fetch-access-token.service.js";
-import { requestErrorResponse } from "../utilities/request-response.utility.js";
-import { verifyAndParseShopifyRedirectRequest } from "../utilities/shopify-redirect-request.utility.js";
-import { paymentAppConfigure } from "../services/payment-app-configure.service.js";
+import { PrismaClient } from '@prisma/client'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { AppRedirectQueryParam } from '../models/redirect-query-params.model.js'
+import { fetchAccessToken } from '../services/fetch-access-token.service.js'
+import { requestErrorResponse } from '../utilities/request-response.utility.js'
+import { verifyAndParseShopifyRedirectRequest } from '../utilities/shopify-redirect-request.utility.js'
+import { paymentAppConfigure } from '../services/payment-app-configure.service.js'
 
 export const redirect = async (
-  event: APIGatewayProxyEvent
+    event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const prisma = new PrismaClient();
+    const prisma = new PrismaClient()
 
-  let parsedAppRedirectQuery: AppRedirectQueryParam;
+    let parsedAppRedirectQuery: AppRedirectQueryParam
 
-  // Verify the security of the request given to install the shopify app
-  try {
-    parsedAppRedirectQuery = await verifyAndParseShopifyRedirectRequest(
-      event.queryStringParameters
-    );
-  } catch (error: unknown) {
-    return requestErrorResponse(error);
-  }
+    // Verify the security of the request given to install the shopify app
+    try {
+        parsedAppRedirectQuery = await verifyAndParseShopifyRedirectRequest(
+            event.queryStringParameters
+        )
+    } catch (error: unknown) {
+        return requestErrorResponse(error)
+    }
 
-  const shop = parsedAppRedirectQuery.shop;
-  const code = parsedAppRedirectQuery.code;
+    const shop = parsedAppRedirectQuery.shop
+    const code = parsedAppRedirectQuery.code
 
-  try {
-    var accessTokenResponse = await fetchAccessToken(shop, code);
-  } catch (error: unknown) {
-    return requestErrorResponse(error);
-  }
+    try {
+        var accessTokenResponse = await fetchAccessToken(shop, code)
+    } catch (error: unknown) {
+        return requestErrorResponse(error)
+    }
 
-  await prisma.merchant.update({
-    where: {
-      shop: shop,
-    },
-    data: {
-      accessToken: accessTokenResponse.access_token,
-      scopes: accessTokenResponse.scope,
-    },
-  });
+    await prisma.merchant.update({
+        where: {
+            shop: shop,
+        },
+        data: {
+            accessToken: accessTokenResponse.access_token,
+            scopes: accessTokenResponse.scope,
+        },
+    })
 
-  const configure = await paymentAppConfigure(
-    "greatMerchant123",
-    true,
-    shop,
-    accessTokenResponse.access_token
-  );
+    const configure = await paymentAppConfigure(
+        'greatMerchant123',
+        true,
+        shop,
+        accessTokenResponse.access_token
+    )
 
-  const redirectUrl = `https://www.apple.com/`;
+    const redirectUrl = `https://www.apple.com/`
 
-  return {
-    statusCode: 200,
-    // headers: {
-    //     Location: redirectUrl,
-    //     'Content-Type': 'text/html',
-    // },
-    body: JSON.stringify(
-      {
-        message: configure,
-      },
-      null,
-      2
-    ),
-  };
-};
+    return {
+        statusCode: 200,
+        // headers: {
+        //     Location: redirectUrl,
+        //     'Content-Type': 'text/html',
+        // },
+        body: JSON.stringify(
+            {
+                message: configure,
+            },
+            null,
+            2
+        ),
+    }
+}
