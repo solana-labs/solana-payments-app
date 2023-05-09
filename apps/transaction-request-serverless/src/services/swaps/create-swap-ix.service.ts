263abc0fadccec9f1d1a6067f1985ec7a173e865
littlejohntj <tjl13c@my.fsu.edu>
2023-05-08 20:03:06 -0400

    only god can save me

--- apps/transaction-request-serverless/src/services/swaps/create-swap-ix.service.ts
+++ apps/transaction-request-serverless/src/services/swaps/create-swap-ix.service.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import * as anchor from "@project-serum/anchor";
-import { createJupiterSwapIx } from "./jupiter.service.js";
-
-export type SwapProvider = "jupiter";
-
-export interface SwapIxConfig {
-  provider: SwapProvider;
-  quantity: number;
-  swapingWallet: anchor.web3.PublicKey;
-  fromMint: anchor.web3.PublicKey;
-  toMint: anchor.web3.PublicKey;
-}
-
-export const createSwapIx = async (
-  config: SwapIxConfig
-): Promise<anchor.web3.TransactionInstruction[]> => {
-  switch (config.provider) {
-    case "jupiter":
-      return createJupiterSwapIx(config);
-  }
-};
-=======
 import * as anchor from '@project-serum/anchor'
 import { createJupiterSwapIx } from './jupiter.service.js'
 
@@ -42,4 +19,3 @@ export const createSwapIx = async (
             return createJupiterSwapIx(config)
     }
 }
->>>>>>> main
