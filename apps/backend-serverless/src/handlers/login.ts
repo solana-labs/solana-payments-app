f8b00c8aaf02ecbc484d721099c58a9b13b9d735
littlejohntj <tjl13c@my.fsu.edu>
2023-05-13 12:58:15 -0400

    added types for jwtwebtoken

--- apps/backend-serverless/src/handlers/login.ts
+++ apps/backend-serverless/src/handlers/login.ts
@@ -5,24 +5,24 @@ import { createCookieHeader } from '../utilities/create-cookie-header.utility.js
 // this is a testing function so I can log in as a mock user.
 // should be deleted later, and is just a temporary APIGatewayProxyEvent
 
-export const login = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-    const id = 'merchantid';
+// export const login = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+//     const id = 'merchantid';
 
-    const token = jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
-        expiresIn: '1d',
-    });
+//     // const token = jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+//     //     expiresIn: '1d',
+//     // });
 
-    const cookieHeader = createCookieHeader('authToken', token);
+//     const cookieHeader = createCookieHeader('authToken', token);
 
-    return {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'text/plain',
-            // Location: redirectUrl,
-            // 'Content-Type': 'text/html',
-            'Set-Cookie': cookieHeader,
-        },
-        // cookies: [cookieHeader],
-        body: 'JWT token 1 set as a cookie',
-    };
-};
+//     return {
+//         statusCode: 200,
+//         headers: {
+//             'Content-Type': 'text/plain',
+//             // Location: redirectUrl,
+//             // 'Content-Type': 'text/html',
+//             'Set-Cookie': cookieHeader,
+//         },
+//         // cookies: [cookieHeader],
+//         body: 'JWT token 1 set as a cookie',
+//     };
+// };
