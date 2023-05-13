82dcae5d81c5ae24d4498a46f0798ca3f19332a6
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-05-12 23:57:19 -0400

    Added next pkg and endpoints

--- apps/merchant-ui/src/components/ShopifySignIn.tsx
+++ apps/merchant-ui/src/components/ShopifySignIn.tsx
@@ -4,21 +4,36 @@ interface Props {
     className?: string;
 }
 
-// TODO: Implement the sign in flow
 export function ShopifySignIn(props: Props) {
     return (
-        <button
-            className={twMerge(
-                'border-gray-300',
-                'border',
-                'font-semibold',
-                'h-11',
-                'rounded-lg',
-                'text-slate-700',
-                props.className
-            )}
-        >
-            Sign in with Shopify
-        </button>
+        <>
+            {' '}
+            <button
+                className={twMerge(
+                    'border-gray-300',
+                    'border',
+                    'font-semibold',
+                    'h-11',
+                    'rounded-lg',
+                    'text-slate-700',
+                    props.className
+                )}
+            >
+                Sign in with Shopify
+            </button>
+            <button
+                className={twMerge(
+                    'border-gray-300',
+                    'border',
+                    'font-semibold',
+                    'h-11',
+                    'rounded-lg',
+                    'text-slate-700',
+                    props.className
+                )}
+            >
+                Sign in Temporary
+            </button>
+        </>
     );
 }
---
+++ apps/merchant-ui/src/lib/endpoints.ts
@@ -0,0 +1,7 @@
+const API_BASE_URL = 'https://ehfytczade.execute-api.us-east-1.amazonaws.com';
+const API_ENDPOINTS = {
+    install: `${API_BASE_URL}/install`,
+    redirect: `${API_BASE_URL}/redirect`,
+    payment: `${API_BASE_URL}/payment`,
+    // ... (define the rest of your endpoints here)
+};
