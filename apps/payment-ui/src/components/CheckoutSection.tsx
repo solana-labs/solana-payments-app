10480e7a6844d8a896140f6ae4721c375199c808
littlejohntj <tjl13c@my.fsu.edu>
2023-05-21 23:30:47 -0400

    ok the screen is there, just need to make it kick off a timer and test with real payments

--- apps/payment-ui/src/components/CheckoutSection.tsx
+++ apps/payment-ui/src/components/CheckoutSection.tsx
@@ -12,34 +12,17 @@ import { createQR } from './SolanaPayQRCode';
 import WalletButton from './WalletButton';
 import { HiOutlineCheckCircle } from 'react-icons/hi';
 import Image from 'next/image';
+import { ThankYouView } from './ThankYou';
+import { PaymentView } from './PaymentView';
 
 const CheckoutSection = () => {
-    const dispatch = useDispatch<AppDispatch>();
     const paymentMethod = useSelector(getPaymentMethod);
+    const done = true
 
     return (
         // <div className="w-full mx-auto rounded-t-xl bg-white flex flex-col justify-between sm:h-[95vh] h-[90vh] sm:px-16 pt-16 px-4"></div>
-        <div className="w-full mx-auto rounded-t-xl bg-white flex flex-col justify-between sm:h-[95vh] h-[90vh] sm:px-16 px-4">
-            <div className="flex flex-col">
-                <Image src="/check.svg" alt="Completed Payment" className="mx-auto w-16 h-16 mt-7 mb-4" width={10} height={10} />
-                <div className="text-3xl text-black mx-auto">{"Thanks for your order."}</div>
-                <div className="text-1xl text-gray-600 mx-auto pt-4">{"Your payment was successful."}</div>
-                <div className="divider w-full" />
-                <div className="text-sm text-gray-400 font-light mx-auto">{"We're redirecting you back to Shopify..."}</div>
-            </div>
-            
-            
-            {/* <div className="w-full flex flex-col">
-                <div className="relative pb-8 flex-col hidden sm:flex">
-                    <PaymentMethodTab />
-                </div>
-                <div className="relative flex flex-col">
-                    <PayToLabel />
-                </div>
-            </div>
-            <div className="relative flex flex-col h-full">
-                {paymentMethod == 'connect-wallet' ? <PayWithWalletSection /> : <PayWithQRCodeSection />}
-            </div> */}
+        <div className="w-full mx-auto rounded-t-xl bg-white  sm:h-[95vh] h-[90vh] sm:px-16 px-4">
+            { done ? <ThankYouView /> : <PaymentView /> }
         </div>
     );
 };
---
+++ apps/payment-ui/src/components/ThankYou.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export const ThankYouView = () => {
+    return (
+        <div className="flex flex-col">
+            <Image src="/check.svg" alt="Completed Payment" className="mx-auto w-16 h-16 mt-7 mb-4" width={10} height={10} />
+            <div className="text-3xl text-black mx-auto">{"Thanks for your order."}</div>
+            <div className="text-1xl text-gray-600 mx-auto pt-4">{"Your payment was successful."}</div>
+            <div className="divider w-full" />
+            <div className="text-sm text-gray-400 font-light mx-auto">{"We're redirecting you back to Shopify..."}</div>
+        </div>
+    )
+}
