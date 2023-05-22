32c19814356e816616ddc06ac93ee7aab28a9512
littlejohntj <tjl13c@my.fsu.edu>
2023-05-21 22:50:35 -0400

    the screen looks good

--- apps/payment-ui/src/components/CheckoutSection.tsx
+++ apps/payment-ui/src/components/CheckoutSection.tsx
@@ -10,14 +10,26 @@ import PayWithWalletSection from './PayWithWalletSection';
 import { QRCode } from './QRCode';
 import { createQR } from './SolanaPayQRCode';
 import WalletButton from './WalletButton';
+import { HiOutlineCheckCircle } from 'react-icons/hi';
+import Image from 'next/image';
 
 const CheckoutSection = () => {
     const dispatch = useDispatch<AppDispatch>();
     const paymentMethod = useSelector(getPaymentMethod);
 
     return (
-        <div className="w-full mx-auto rounded-t-xl bg-white flex flex-col justify-between sm:h-[95vh] h-[90vh] sm:px-16 pt-16 px-4">
-            <div className="w-full flex flex-col">
+        // <div className="w-full mx-auto rounded-t-xl bg-white flex flex-col justify-between sm:h-[95vh] h-[90vh] sm:px-16 pt-16 px-4"></div>
+        <div className="w-full mx-auto rounded-t-xl bg-white flex flex-col justify-between sm:h-[95vh] h-[90vh] sm:px-16 px-4">
+            <div className="flex flex-col">
+                <Image src="/check.svg" alt="Completed Payment" className="mx-auto w-16 h-16 mt-7 mb-4" width={10} height={10} />
+                <div className="text-3xl text-black mx-auto">{"Thanks for your order."}</div>
+                <div className="text-1xl text-gray-600 mx-auto pt-4">{"Your payment was successful."}</div>
+                <div className="divider w-full" />
+                <div className="text-sm text-gray-400 font-light mx-auto">{"We're redirecting you back to Shopify..."}</div>
+            </div>
+            
+            
+            {/* <div className="w-full flex flex-col">
                 <div className="relative pb-8 flex-col hidden sm:flex">
                     <PaymentMethodTab />
                 </div>
@@ -27,7 +39,7 @@ const CheckoutSection = () => {
             </div>
             <div className="relative flex flex-col h-full">
                 {paymentMethod == 'connect-wallet' ? <PayWithWalletSection /> : <PayWithQRCodeSection />}
-            </div>
+            </div> */}
         </div>
     );
 };
