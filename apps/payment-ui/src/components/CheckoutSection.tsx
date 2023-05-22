7473178fdb34f9b2790dd079dc60a576c31265e1
littlejohntj <tjl13c@my.fsu.edu>
2023-05-22 13:14:40 -0400

    going good, we have the view set up. just need to figure out how to show

--- apps/payment-ui/src/components/CheckoutSection.tsx
+++ apps/payment-ui/src/components/CheckoutSection.tsx
@@ -14,6 +14,7 @@ import { HiOutlineCheckCircle } from 'react-icons/hi';
 import Image from 'next/image';
 import { ThankYouView } from './ThankYou';
 import { PaymentView } from './PaymentView';
+import { ErrorGoBack } from './ErrorGoBack';
 
 const CheckoutSection = () => {
     const paymentDetails = useSelector(getPaymentDetails);
@@ -21,7 +22,8 @@ const CheckoutSection = () => {
     return (
         // <div className="w-full mx-auto rounded-t-xl bg-white flex flex-col justify-between sm:h-[95vh] h-[90vh] sm:px-16 pt-16 px-4"></div>
         <div className="w-full mx-auto rounded-t-xl bg-white  sm:h-[95vh] h-[90vh] sm:px-16 px-4">
-            { paymentDetails.redirectUrl != null ? <ThankYouView /> : <PaymentView /> }
+            <ErrorGoBack top='Your session timed out.' bottom='Please go back and checkout again.' />
+            {/* { paymentDetails.redirectUrl != null ? <ThankYouView /> : <PaymentView /> } */}
         </div>
     );
 };
---
+++ apps/payment-ui/src/components/ErrorGoBack.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import FinishHandler from './FinishHandler'
+import { VscArrowLeft } from 'react-icons/vsc'
+import { ImWarning } from 'react-icons/im'
+
+export const ErrorGoBack = ( props: { top: string, bottom: string } ) => {
+    return (
+        <div className="flex flex-col">
+            <ErrorDisplay top={props.top} bottom={props.bottom} />
+            <GoBackButton />
+        </div>
+    )
+}
+
+const ErrorDisplay = ( props: { top: string, bottom: string } ) => {
+    return (
+        <div className='rounded-lg outline outline-1 mt-16 h-16 outline-orange-600 bg-orange-100 flex flex-row items-start'>
+            <div className='flex flex-col h-full justify-center'>
+                <ImWarning className='text-orange-600 ml-4 mr-3 text-md' />
+                <div className="text-md text-orange-100 font-light">{'.'}</div>
+            </div>
+            <div className='flex flex-col h-full justify-center'>
+                <div className="text-sm text-orange-800 font-semibold">{props.top}</div>
+                <div className="text-sm text-orange-800 font-light">{props.bottom}</div>
+            </div>
+        </div>
+    )
+}
+
+const GoBackButton = () => {
+    return (
+        <button className='btn btn-ghost outline outline-offset-0 text-black normal-case outline-2 mt-4'>
+            <VscArrowLeft className='w-6 h-6 pr-1' />
+            <div className='pl-1 text-md'>Go back</div>
+        </button>
+    )
+}
