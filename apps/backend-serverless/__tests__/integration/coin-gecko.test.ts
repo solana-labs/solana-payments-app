7bcaac67f895c6d346948c247e9f0d7c82cf44ed
littlejohntj <tjl13c@my.fsu.edu>
2023-05-22 17:45:10 -0400

    removed comments

--- apps/backend-serverless/__tests__/integration/coin-gecko.test.ts
+++ apps/backend-serverless/__tests__/integration/coin-gecko.test.ts
@@ -1,12 +1,12 @@
 import { convertAmountAndCurrencyToUsdcSize } from '../../src/services/coin-gecko.service.js';
 import { web3 } from '@project-serum/anchor';
 
-// const hashIntoPublicKey = async (inputs: string[]) => {
-//     return await web3.PublicKey.findProgramAddressSync(
-//         inputs.map(input => Buffer.from(input)),
-//         web3.SystemProgram.programId
-//     )[0];
-// };
+const hashIntoPublicKey = async (inputs: string[]) => {
+    return await web3.PublicKey.findProgramAddressSync(
+        inputs.map(input => Buffer.from(input)),
+        web3.SystemProgram.programId
+    )[0];
+};
 
 describe('integration testing coin gecko api', () => {
     it('valid', async () => {
