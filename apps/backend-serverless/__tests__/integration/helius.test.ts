e220e71ea2c3149476f7979f8b47fd92b57e7667
littlejohntj <tjl13c@my.fsu.edu>
2023-05-23 12:42:37 -0400

    we now have all of the gdpr handlers parsing headers and have that method testing

--- apps/backend-serverless/__tests__/integration/helius.test.ts
+++ apps/backend-serverless/__tests__/integration/helius.test.ts
@@ -5,15 +5,15 @@ describe('integration testing coin gecko api', () => {
         // const result = await convertAmountAndCurrencyToUsdcSize(10, 'USD');
         // expect(result).toBeCloseTo(10);
         // commented out because it's not working, but it's not a big deal
-        process.env.HELIUS_API_KEY = '5f70b753-57cb-422b-a018-d7df67b4470e';
-        try {
-            const tx = await fetchEnhancedTransaction(
-                '5mbRQYwPrrj3DFdt8mf1xd4UDxewfQhvU4Au2zT1x1rcRy7J1dWtL4GrY5tds9B35GP7w4mfZRzXmNKaeCrjP7w2'
-            );
-            console.log(tx);
-        } catch (error) {
-            console.log(error);
-        }
+        // process.env.HELIUS_API_KEY = '5f70b753-57cb-422b-a018-d7df67b4470e';
+        // try {
+        //     const tx = await fetchEnhancedTransaction(
+        //         '5mbRQYwPrrj3DFdt8mf1xd4UDxewfQhvU4Au2zT1x1rcRy7J1dWtL4GrY5tds9B35GP7w4mfZRzXmNKaeCrjP7w2'
+        //     );
+        //     console.log(tx);
+        // } catch (error) {
+        //     console.log(error);
+        // }
         expect(true).toBe(true);
     });
 });
--- apps/backend-serverless/src/models/shopify-webhook-headers.model.ts
+++ apps/backend-serverless/src/models/shopify-webhook-headers.model.ts
@@ -1,5 +1,5 @@
 import { object, string, InferType } from 'yup';
-import { parseAndValidate } from '../utilities/yup.utility.js';
+import { parseAndValidate, parseAndValidateStrict } from '../utilities/yup.utility.js';
 import { camelCase } from 'lodash';
 
 function toCamelCase(obj: Record<string, string>): Record<string, string> {
@@ -18,19 +18,30 @@ export enum ShopifyWebhookTopic {
     shopReact = 'shop/redact',
 }
 
-export const shopifyWebhookHeadersScheme = object().shape({
-    'X-Shopify-Topic': string().oneOf(Object.values(ShopifyWebhookTopic), 'Invalid shopify topic type').required(),
-    'X-Shopify-Hmac-Sha256': string().required(),
-    'X-Shopify-Shop-Domain': string().required(),
-    'X-Shopify-API-Version': string().required(),
-    'X-Shopify-Webhook-Id': string().required(),
-    'X-Shopify-Triggered-At': string().required(),
-});
+export const shopifyWebhookHeadersScheme = object()
+    .shape({
+        'X-Shopify-Topic': string().oneOf(Object.values(ShopifyWebhookTopic), 'Invalid shopify topic type').required(),
+        'X-Shopify-Hmac-Sha256': string().required(),
+        'X-Shopify-Shop-Domain': string().required(),
+        'X-Shopify-API-Version': string().required(),
+        'X-Shopify-Webhook-Id': string().required(),
+        'X-Shopify-Triggered-At': string().required(),
+    })
+    .transform((_, originalValue) => toCamelCase(originalValue));
 
 export type ShopifyWebhookHeaders = InferType<typeof shopifyWebhookHeadersScheme>;
 
-export const parseAndValidateShopifyWebhookHeaders = (shopifyRequestHeaders: any): ShopifyWebhookHeaders => {
-    return parseAndValidate<ShopifyWebhookHeaders>(
+export type ParsedShopifyWebhookHeaders = {
+    shopifyTopic: string;
+    hmacSha256: string;
+    shopDomain: string;
+    apiVersion: string;
+    webhookId: string;
+    triggeredAt: string;
+};
+
+export const parseAndValidateShopifyWebhookHeaders = (shopifyRequestHeaders: any): ParsedShopifyWebhookHeaders => {
+    return parseAndValidateStrict<ParsedShopifyWebhookHeaders>(
         shopifyRequestHeaders,
         shopifyWebhookHeadersScheme,
         'Could not parse the shopify webhook headers. Unknown Reason.'
