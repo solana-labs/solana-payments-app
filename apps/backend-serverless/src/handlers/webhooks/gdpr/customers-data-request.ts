ec3754da150cccc933f18de6f71e998542aacbdf
littlejohntj <tjl13c@my.fsu.edu>
2023-05-22 23:29:06 -0400

    gonna add in lodash

--- apps/backend-serverless/src/handlers/webhooks/gdpr/customers-data-request.ts
+++ apps/backend-serverless/src/handlers/webhooks/gdpr/customers-data-request.ts
@@ -1,5 +1,10 @@
 import * as Sentry from '@sentry/serverless';
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import {
+    ShopifyWebhookHeaders,
+    parseAndValidateShopifyWebhookHeaders,
+} from '../../../models/shopify-webhook-headers.model.js';
+import { requestErrorResponse } from '../../../utilities/request-response.utility.js';
 
 Sentry.AWSLambda.init({
     dsn: 'https://dbf74b8a0a0e4927b9269aa5792d356c@o4505168718004224.ingest.sentry.io/4505168722526208',
@@ -8,6 +13,16 @@ Sentry.AWSLambda.init({
 
 export const customersDataRequest = Sentry.AWSLambda.wrapHandler(
     async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+        let webhookHeaders: ShopifyWebhookHeaders;
+
+        try {
+            webhookHeaders = parseAndValidateShopifyWebhookHeaders(event.headers);
+        } catch (error) {
+            return requestErrorResponse(error);
+        }
+
+        if ()
+
         return {
             statusCode: 200,
             body: JSON.stringify({}),
