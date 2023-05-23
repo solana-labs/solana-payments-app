b99798500be8fc5793b92ab45242de22cf57e5a9
littlejohntj <tjl13c@my.fsu.edu>
2023-05-23 13:50:23 -0400

    feels good for now

--- apps/backend-serverless/src/handlers/webhooks/gdpr/shop-redact.ts
+++ apps/backend-serverless/src/handlers/webhooks/gdpr/shop-redact.ts
@@ -7,6 +7,9 @@ import {
 } from '../../../models/shopify-webhook-headers.model.js';
 import { requestErrorResponse } from '../../../utilities/request-response.utility.js';
 import { verifyShopifyWebhook } from '../../../utilities/verify-shopify-webhook-header.utility.js';
+import { ShopRedactRequest, parseAndValidateShopRedactRequestBody } from '../../../models/shop-redact-request.model.js';
+import { Merchant, PrismaClient } from '@prisma/client';
+import { MerchantService } from '../../../services/database/merchant-service.database.service.js';
 
 Sentry.AWSLambda.init({
     dsn: 'https://dbf74b8a0a0e4927b9269aa5792d356c@o4505168718004224.ingest.sentry.io/4505168722526208',
@@ -16,6 +19,8 @@ Sentry.AWSLambda.init({
 export const shopRedact = Sentry.AWSLambda.wrapHandler(
     async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
         let webhookHeaders: ParsedShopifyWebhookHeaders;
+        const prisma = new PrismaClient();
+        const merchantService = new MerchantService(prisma);
 
         try {
             webhookHeaders = parseAndValidateShopifyWebhookHeaders(event.headers);
@@ -38,6 +43,23 @@ export const shopRedact = Sentry.AWSLambda.wrapHandler(
         } catch (error) {
             return requestErrorResponse(error);
         }
+
+        let shopReactRequest: ShopRedactRequest;
+
+        try {
+            shopReactRequest = parseAndValidateShopRedactRequestBody(event.body);
+        } catch (error) {
+            return requestErrorResponse(error);
+        }
+
+        const merchant = await merchantService.getMerchant({ shop: shopReactRequest.shop_domain });
+
+        if (merchant == null) {
+            return requestErrorResponse(new Error('Merchant not found'));
+        }
+
+        // At this point we would have the merchant record we need and we would
+        // either save this into a queue or just perform the delete now
 
         return {
             statusCode: 200,
