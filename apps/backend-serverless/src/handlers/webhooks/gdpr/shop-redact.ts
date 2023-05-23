b07c8c4a3fac7b30f3f1b3e887637c895eb41744
littlejohntj <tjl13c@my.fsu.edu>
2023-05-22 22:54:16 -0400

    set up all of the gdpr webhook handlers

---
+++ apps/backend-serverless/src/handlers/webhooks/gdpr/shop-redact.ts
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/serverless';
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+
+Sentry.AWSLambda.init({
+    dsn: 'https://dbf74b8a0a0e4927b9269aa5792d356c@o4505168718004224.ingest.sentry.io/4505168722526208',
+    tracesSampleRate: 1.0,
+});
+
+export const shopRedact = Sentry.AWSLambda.wrapHandler(
+    async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({}),
+        };
+    },
+    {
+        rethrowAfterCapture: true,
+    }
+);
