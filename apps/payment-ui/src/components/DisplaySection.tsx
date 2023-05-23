2c871ec6a4e09adcd9c13b285a313c619dbb6b92
TJ Littlejohn <tjl13c@my.fsu.edu>
2023-05-23 19:52:36 -0400

    86 make the back button functioning on payment UI (#87)
    * the back icon is now a button
    
    * ok back button should work

--- apps/payment-ui/src/components/DisplaySection.tsx
+++ apps/payment-ui/src/components/DisplaySection.tsx
@@ -1,12 +1,26 @@
 import { MdArrowBack } from 'react-icons/md';
 import Image from 'next/image';
+import { getPaymentDetails } from '@/features/pay-tab/paySlice';
+import { useSelector } from 'react-redux';
 
 const DisplaySection = () => {
+
+    const paymentDetails = useSelector(getPaymentDetails);
+
     return (
         <div className="flex flex-row w-full sm:h-[5vh] h-[10vh]">
             <div className="w-full relative flex flex-row items-center justify-center">
                 <div className="absolute flex flex-row justify-start w-full sm:px-8 px-4">
+                <button
+                    className="btn btn-ghost z-10"
+                    onClick={() => {
+                        if ( paymentDetails?.cancelUrl != null ) {
+                            window.location.href = paymentDetails.cancelUrl;
+                        }
+                    }}
+                >
                     <MdArrowBack color="white" size={30} />
+                </button>
                 </div>
                 <div className="absolute flex flex-row justify-center w-full">
                     <Image src="/solana-pay.svg" alt="Solana Pay Logo" width={80} height={200} />
