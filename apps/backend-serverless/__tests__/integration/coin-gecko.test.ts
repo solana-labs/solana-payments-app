61276be83e503469bc2a354bd8651ee568fb011e
littlejohntj <tjl13c@my.fsu.edu>
2023-05-25 17:24:00 -0400

    migrated the logic to its own service

--- apps/backend-serverless/__tests__/integration/coin-gecko.test.ts
+++ apps/backend-serverless/__tests__/integration/coin-gecko.test.ts
@@ -1,7 +1,12 @@
 import { convertAmountAndCurrencyToUsdcSize } from '../../src/services/coin-gecko.service.js';
 import { web3 } from '@project-serum/anchor';
-import fetch from 'node-fetch';
-import { decode } from 'bs58';
+import { fetchTransaction } from '../../src/services/fetch-transaction.service.js';
+const hashIntoPublicKey = async (inputs: string[]) => {
+    return await web3.PublicKey.findProgramAddressSync(
+        inputs.map(input => Buffer.from(input)),
+        web3.SystemProgram.programId
+    )[0];
+};
 
 describe('integration testing coin gecko api', () => {
     it('valid', async () => {
@@ -13,30 +18,9 @@ describe('integration testing coin gecko api', () => {
         // const txSig = '5Yt9vFGDwXWMPqgXkhocNB77F7FasrHJrmEtL1UoU9X3G7Lx5LC8yCaxfqJ7TZ4wrHfHrfYPBcjvkGfWwoLNzE8A';
         // const response = await connection.getTransaction(txSig, { encoding: 'base58' });
         // console.log(response);
-        const {
-            result: {
-                transaction: [bs58Transaction],
-            },
-        } = await (
-            await fetch('https://rpc.helius.xyz/?api-key=5f70b753-57cb-422b-a018-d7df67b4470e', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    jsonrpc: '2.0',
-                    method: 'getTransaction',
-                    id: '1',
-                    params: [
-                        '5Yt9vFGDwXWMPqgXkhocNB77F7FasrHJrmEtL1UoU9X3G7Lx5LC8yCaxfqJ7TZ4wrHfHrfYPBcjvkGfWwoLNzE8A',
-                        { encoding: 'base58' },
-                    ],
-                }),
-            })
-        ).json();
 
-        const decodedTransaction = decode(bs58Transaction);
-
-        const tx = web3.Transaction.from(decodedTransaction);
-
+        const txId = '5Yt9vFGDwXWMPqgXkhocNB77F7FasrHJrmEtL1UoU9X3G7Lx5LC8yCaxfqJ7TZ4wrHfHrfYPBcjvkGfWwoLNzE8A';
+        const tx = await fetchTransaction(txId);
         console.log(tx);
     });
 });
---
+++ apps/backend-serverless/src/services/fetch-transaction.service.ts
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+import { web3 } from '@project-serum/anchor';
+import { decode } from 'bs58';
+
+export const fetchTransaction = async (transactionId: string): Promise<web3.Transaction> => {
+    const {
+        result: {
+            transaction: [bs58Transaction],
+        },
+    } = await (
+        await fetch('https://rpc.helius.xyz/?api-key=5f70b753-57cb-422b-a018-d7df67b4470e', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                jsonrpc: '2.0',
+                method: 'getTransaction',
+                id: '1',
+                params: [transactionId, { encoding: 'base58' }],
+            }),
+        })
+    ).json();
+
+    const decodedTransaction = decode(bs58Transaction);
+
+    const tx = web3.Transaction.from(decodedTransaction);
+
+    return tx;
+};
