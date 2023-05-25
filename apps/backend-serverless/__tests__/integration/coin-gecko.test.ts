9fbe1754c1cf915b18a4a73da8e1bd3761deb3d5
littlejohntj <tjl13c@my.fsu.edu>
2023-05-25 17:13:34 -0400

    web3.Transaction object is being fetched in the tests

--- apps/backend-serverless/__tests__/integration/coin-gecko.test.ts
+++ apps/backend-serverless/__tests__/integration/coin-gecko.test.ts
@@ -1,5 +1,7 @@
 import { convertAmountAndCurrencyToUsdcSize } from '../../src/services/coin-gecko.service.js';
 import { web3 } from '@project-serum/anchor';
+import fetch from 'node-fetch';
+import { decode } from 'bs58';
 
 const hashIntoPublicKey = async (inputs: string[]) => {
     return await web3.PublicKey.findProgramAddressSync(
@@ -13,5 +15,35 @@ describe('integration testing coin gecko api', () => {
         // const result = await convertAmountAndCurrencyToUsdcSize(10, 'USD');
         // expect(result).toBeCloseTo(10);
         // commented out because it's not working, but it's not a big deal
+
+        // const connection = new web3.Connection('https://rpc.helius.xyz/?api-key=5f70b753-57cb-422b-a018-d7df67b4470e');
+        // const txSig = '5Yt9vFGDwXWMPqgXkhocNB77F7FasrHJrmEtL1UoU9X3G7Lx5LC8yCaxfqJ7TZ4wrHfHrfYPBcjvkGfWwoLNzE8A';
+        // const response = await connection.getTransaction(txSig, { encoding: 'base58' });
+        // console.log(response);
+        const {
+            result: {
+                transaction: [bs58Transaction],
+            },
+        } = await (
+            await fetch('https://rpc.helius.xyz/?api-key=5f70b753-57cb-422b-a018-d7df67b4470e', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'getTransaction',
+                    id: '1',
+                    params: [
+                        '5Yt9vFGDwXWMPqgXkhocNB77F7FasrHJrmEtL1UoU9X3G7Lx5LC8yCaxfqJ7TZ4wrHfHrfYPBcjvkGfWwoLNzE8A',
+                        { encoding: 'base58' },
+                    ],
+                }),
+            })
+        ).json();
+
+        const decodedTransaction = decode(bs58Transaction);
+
+        const tx = web3.Transaction.from(decodedTransaction);
+
+        console.log(tx);
     });
 });
