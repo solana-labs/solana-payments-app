e9360fe29e3e5c537f9192d47554146d570a6cfe
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-05-25 16:42:16 -0400

    returning date in sls handler

--- apps/backend-serverless/src/utilities/payment-record.utility.ts
+++ apps/backend-serverless/src/utilities/payment-record.utility.ts
@@ -2,15 +2,18 @@ import { PaymentRecord } from '@prisma/client';
 
 export interface PaymentDataResponse {
     shopifyOrder: string;
-    date: string;
+    requestedAt: Date;
+    completedAt?: Date;
     status: string;
     amount: string;
 }
 
 export const createPaymentDataResponseFromPaymentRecord = (paymentRecord: PaymentRecord): PaymentDataResponse => {
+    console.log('date', paymentRecord.requestedAt, paymentRecord.completedAt);
     return {
         shopifyOrder: paymentRecord.shopId,
-        date: 'some-date-here',
+        requestedAt: paymentRecord.requestedAt,
+        ...(paymentRecord.completedAt && { completedAt: paymentRecord.completedAt }),
         status: paymentRecord.status,
         amount: paymentRecord.amount ? `${paymentRecord.amount} ${paymentRecord.currency}` : 'Not Availible',
     };
