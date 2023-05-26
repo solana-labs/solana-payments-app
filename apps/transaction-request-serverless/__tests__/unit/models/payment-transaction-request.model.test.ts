c61b9cbb29476628e6387217ee3411ef9f0a1965
littlejohntj <tjl13c@my.fsu.edu>
2023-05-26 19:35:19 -0400

    we have optional parameters

---
+++ apps/transaction-request-serverless/__tests__/unit/models/payment-transaction-request.model.test.ts
@@ -0,0 +1,76 @@
+import {
+    AmountType,
+    TransactionType,
+    parseAndValidatePaymentTransactionRequest,
+} from '../../../src/models/payment-transaction-request.model.js';
+import { web3 } from '@project-serum/anchor';
+
+describe('unit testing the payment transaction request model', () => {
+    it('valid transaction request model', () => {
+        const receiver = web3.Keypair.generate().publicKey.toBase58();
+        const feePayer = web3.Keypair.generate().publicKey.toBase58();
+        const singleUseNewAcc = web3.Keypair.generate().publicKey.toBase58();
+        const singleUsePayer = web3.Keypair.generate().publicKey.toBase58();
+
+        const mockTransactionRequestQueryParameters = {
+            receiver: receiver,
+            sendingToken: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+            receivingToken: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+            feePayer: feePayer,
+            receivingAmount: 1,
+            amountType: AmountType.size,
+            transactionType: TransactionType.blockhash,
+            createAta: true,
+            singleUseNewAcc: singleUseNewAcc,
+            singleUsePayer: singleUsePayer,
+            indexInputs: 'hello,world',
+        };
+
+        expect(() => {
+            parseAndValidatePaymentTransactionRequest(mockTransactionRequestQueryParameters);
+        }).not.toThrow();
+    });
+
+    it('valid transaction request model, defaults removed', () => {
+        const receiver = web3.Keypair.generate().publicKey.toBase58();
+        const feePayer = web3.Keypair.generate().publicKey.toBase58();
+        const singleUseNewAcc = web3.Keypair.generate().publicKey.toBase58();
+        const singleUsePayer = web3.Keypair.generate().publicKey.toBase58();
+
+        const mockTransactionRequestQueryParameters = {
+            receiver: receiver,
+            sendingToken: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+            receivingToken: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+            feePayer: feePayer,
+            receivingAmount: 1,
+            singleUseNewAcc: singleUseNewAcc,
+            singleUsePayer: singleUsePayer,
+            indexInputs: 'hello,world',
+        };
+
+        expect(() => {
+            parseAndValidatePaymentTransactionRequest(mockTransactionRequestQueryParameters);
+        }).not.toThrow();
+    });
+
+    it('invalid transaction request model, missing sending token', () => {
+        const receiver = web3.Keypair.generate().publicKey.toBase58();
+        const feePayer = web3.Keypair.generate().publicKey.toBase58();
+        const singleUseNewAcc = web3.Keypair.generate().publicKey.toBase58();
+        const singleUsePayer = web3.Keypair.generate().publicKey.toBase58();
+
+        const mockTransactionRequestQueryParameters = {
+            receiver: receiver,
+            receivingToken: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+            feePayer: feePayer,
+            receivingAmount: 1,
+            singleUseNewAcc: singleUseNewAcc,
+            singleUsePayer: singleUsePayer,
+            indexInputs: 'hello,world',
+        };
+
+        expect(() => {
+            parseAndValidatePaymentTransactionRequest(mockTransactionRequestQueryParameters);
+        }).toThrow();
+    });
+});
