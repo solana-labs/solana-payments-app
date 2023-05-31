9d894d42797c1cca028e3eabb19a26d85cae466e
littlejohntj <tjl13c@my.fsu.edu>
2023-05-31 17:20:48 -0400

    dded another test

---
+++ apps/backend-serverless/__tests__/unit/services/shopify/admin-data.service.test.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { makeAdminData } from '../../../../src/services/shopify/admin-data.service.js';
+import { createMockAdminDataResponse } from '../../../../src/utilities/testing-helper/create-mock.utility';
+
+describe('unit testing payment app configure', () => {
+    it('successful response', async () => {
+        let mock = new MockAdapter(axios);
+        const mockAdminDataResponse = createMockAdminDataResponse();
+        mock.onPost().reply(200, mockAdminDataResponse);
+        const mockAdminData = makeAdminData(axios);
+
+        await expect(mockAdminData('mock-shop', 'mock-token')).resolves.not.toThrow();
+    });
+});
