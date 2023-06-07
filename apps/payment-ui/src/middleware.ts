2e3a67c0c6efb6f5b67956fd823cbbce281649ae
littlejohntj <tjl13c@my.fsu.edu>
2023-06-07 13:59:03 -0400

    got geo blocking working

---
+++ apps/payment-ui/src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { bool } from 'yup';
+
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = 'US';
+
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+    matcher: '/secret-page',
+};
+
+const isBlockedGeo = (request: NextRequest): boolean => {
+    const BLOCKED_COUNTRY = 'US';
+
+    const geo = request.geo;
+
+    if (geo == null || geo.country == null) {
+        return true;
+    }
+
+    if (geo.country === 'CN') {
+        return true;
+    } else if (geo.country === 'US' && geo.region === 'NY') {
+        return true;
+    }
+
+    return true;
+
+    return false;
+};
+
+export function middleware(request: NextRequest) {
+    if (isBlockedGeo(request)) {
+        request.nextUrl.pathname = '/login';
+    }
+
+    // Rewrite to URL
+    return NextResponse.rewrite(request.nextUrl);
+}
