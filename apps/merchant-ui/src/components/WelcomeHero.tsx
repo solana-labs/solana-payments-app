ca7bfc1856e4a36d4158cc9785d80e14a4fb0f10
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-07 00:54:03 -0400

    Images and Icons in Merchant UI (#229)
    * Added icons to getting started
    
    * added hero image

--- apps/merchant-ui/src/components/WelcomeHero.tsx
+++ apps/merchant-ui/src/components/WelcomeHero.tsx
@@ -6,5 +6,13 @@ interface Props {
 }
 
 export function WelcomeHero(props: Props) {
-    return <Image className={twMerge('bg-neutral-50', props.className)} src="" alt="Welcome to Solana Pay" />;
+    return (
+        <Image
+            className={twMerge('bg-neutral-50', props.className)}
+            src="/solana_pay_hero.png"
+            alt="Welcome to Solana Pay"
+            width={400}
+            height={1000}
+        />
+    );
 }
