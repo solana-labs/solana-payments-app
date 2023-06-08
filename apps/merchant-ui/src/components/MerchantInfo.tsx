e0d3fb696b4014b6abff12bff90cfac3e0309dbf
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-08 10:25:45 -0400

    refetching data to update merchant info

--- apps/merchant-ui/src/components/MerchantInfo.tsx
+++ apps/merchant-ui/src/components/MerchantInfo.tsx
@@ -31,16 +31,15 @@ export function MerchantInfo(props: Props) {
         walletAddress: null,
         token: Token.USDC,
     });
-    const [isVerified, setIsVerified] = useState(false);
     const [pending, setPending] = useState(false);
 
     const merchantInfo = useMerchantStore(state => state.merchantInfo);
+    const getMerchantInfo = useMerchantStore(state => state.getMerchantInfo);
 
     const { toast } = useToast();
 
     useEffect(() => {
         if (isOk(merchantInfo)) {
-            console.log('merchantInfo', merchantInfo);
             setFormState({
                 name: merchantInfo.data.name,
                 logoSrc: 'a',
@@ -132,9 +131,11 @@ export function MerchantInfo(props: Props) {
             <footer className="flex items-center justify-end space-x-3 pt-4">
                 <Button.Secondary>Cancel</Button.Secondary>
                 <Button.Primary
-                    onClick={() => {
+                    onClick={async () => {
                         setPending(true);
-                        updateMerchantAddress(formState.walletAddress?.toString());
+                        await updateMerchantAddress(formState.walletAddress?.toString());
+                        await getMerchantInfo();
+
                         toast({
                             title: 'Updated Merchant Address',
                             variant: 'constructive',
