5e1ef2db5eea827ec381a4cfc8a7a26ee4837495
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-08 10:34:24 -0400

    Toast animation and positioning

--- apps/merchant-ui/tailwind.config.js
+++ apps/merchant-ui/tailwind.config.js
@@ -23,6 +23,21 @@ module.exports = {
                     foreground: 'hsl(var(--constructive-foreground))',
                 },
             },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: 0 },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
         },
     },
+    plugins: [require('tailwindcss-animate')],
 };
