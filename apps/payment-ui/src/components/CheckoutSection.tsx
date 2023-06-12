2e2dc754747a5cbb5fdd537fe541b7159465222f
littlejohntj <tjl13c@my.fsu.edu>
2023-06-12 14:06:50 -0400

    oops

--- apps/payment-ui/src/components/CheckoutSection.tsx
+++ apps/payment-ui/src/components/CheckoutSection.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-import { PaymentMethodTab } from '@/features/pay-tab/PaymentMethodTab';
-import { getPaymentDetails, getPaymentMethod, setPaymentMethod, getPaymentErrors, PayError, PaymentDetails } from '@/features/pay-tab/paySlice';
-import { PayToLabel } from '@/features/pay-tab/PayToLabel';
->>>>>>> e9c2b24 (merging in recent changes to demo (#244))
 import { AppDispatch, RootState } from '@/store';
 import React, { ReactNode, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -20,7 +14,6 @@ import { PaymentView } from './PaymentView';
 import { ErrorGoBack } from './ErrorGoBack';
 import { BlockedProps } from '@/pages';
 import { GeoBlockedView } from './GeoBlockedView';
-<<<<<<< HEAD
 import { PaymentLoadingView } from './PaymentLoadingView';
 import { getIsCompleted, getPaymentDetails, getIsProcessing, getIsError, getIsSolanaPayCompleted } from '@/features/payment-session/paymentSessionSlice';
 import { ErrorView } from './ErrorView';
@@ -49,35 +42,5 @@ const CheckoutSection = (props: BlockedProps) => {
     }
 
 }
-=======
-
-const PaymentErrorView = ( props: { payError: PayError } ) => {
-    return <ErrorGoBack top={props.payError.errorTitle} bottom={props.payError.errorDetail} redirect={props.payError.errorRedirect} />
-}
-
-const PaymentDeatilView = ( props: { paymentDetails: PaymentDetails | null } ) => {
-    return ( props.paymentDetails?.redirectUrl != null ? <ThankYouView /> : <PaymentView /> )
-}
-
-const PaymentRootView = ( props: { payError: PayError | null, payDetail: PaymentDetails | null } ) => {
-    return ( props.payError != null ? <PaymentErrorView payError={props.payError} /> : <PaymentDeatilView paymentDetails={props.payDetail} /> )
-}
-
-const BlockedOrNotRootView = ( props: { payError: PayError | null, payDetail: PaymentDetails | null, isBlocked: string } ) => {
-    return ( props.isBlocked == 'true' ? <GeoBlockedView /> : <PaymentRootView payError={props.payError} payDetail={props.payDetail} /> )
-}
-
-const CheckoutSection = (props: BlockedProps) => {
-
-    const paymentDetails = useSelector(getPaymentDetails);
-    const paymentErrors = useSelector(getPaymentErrors);
-
-    return (
-        <div className="w-full mx-auto rounded-t-xl bg-white  sm:h-[95vh] h-[90vh] sm:px-16 px-4">
-            <BlockedOrNotRootView payError={paymentErrors} payDetail={paymentDetails} isBlocked={props.isBlocked} />
-        </div>
-    );
-};
->>>>>>> e9c2b24 (merging in recent changes to demo (#244))
 
 export default CheckoutSection;
