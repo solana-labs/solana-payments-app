b3352d00f4bea970af0cfe1090f32b7af5ae578f
littlejohntj <tjl13c@my.fsu.edu>
2023-06-12 14:09:01 -0400

    wen

--- apps/payment-ui/src/components/GeoBlockedView.tsx
+++ apps/payment-ui/src/components/GeoBlockedView.tsx
@@ -1,10 +1,6 @@
 import { useSelector } from "react-redux";
 import { ErrorGoBack } from "./ErrorGoBack"
-<<<<<<< HEAD
 import { getPaymentDetails } from "@/features/payment-session/paymentSessionSlice";
-=======
-import { getPaymentDetails } from "@/features/pay-tab/paySlice";
->>>>>>> e9c2b24 (merging in recent changes to demo (#244))
 
 export const GeoBlockedView = () => {
 
