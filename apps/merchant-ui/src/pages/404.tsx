537c59071d03554a408c90204d275c676a461c97
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-11 22:25:19 -0400

    Basic 404 page (#262)

---
+++ apps/merchant-ui/src/pages/404.tsx
@@ -0,0 +1,30 @@
+import { DefaultLayout } from '@/components/DefaultLayout';
+import { isOk } from '@/lib/Result';
+import { useMerchantStore } from '@/stores/merchantStore';
+import Head from 'next/head';
+import Router from 'next/router';
+
+export default function Custom404() {
+    const merchantInfo = useMerchantStore(state => state.merchantInfo);
+    if (isOk(merchantInfo) && !merchantInfo.data.completed) {
+        Router.push('/getting-started');
+    }
+    return (
+        <>
+            <Head>
+                <title>Solana Pay - 404</title>
+                <meta name="description" content="404 page" />
+            </Head>
+            <div className="h-screen w-screen">
+                <DefaultLayout accountIsActive className="h-full w-full ">
+                    <div className="flex flex-col justify-center h-full ">
+                        <div className="mt-4 text-center">
+                            <h1 className="text-2xl font-semibold">404</h1>
+                            <p className="text-lg  mt-2">Page not found</p>
+                        </div>
+                    </div>
+                </DefaultLayout>
+            </div>
+        </>
+    );
+}
