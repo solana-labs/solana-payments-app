15c65277d5816e2a0e52e5526188f623fa8d3a00
littlejohntj <tjl13c@my.fsu.edu>
2023-06-11 22:34:53 -0400

    works clean gold

--- apps/backend-serverless/src/handlers/webhooks/helius.ts
+++ apps/backend-serverless/src/handlers/webhooks/helius.ts
@@ -4,16 +4,11 @@ import {
     HeliusEnhancedTransactionArray,
     parseAndValidateHeliusEnchancedTransaction,
 } from '../../models/dependencies/helius-enhanced-transaction.model.js';
-import { PaymentRecord, PrismaClient, RefundRecord, TransactionType, WebsocketSession } from '@prisma/client';
-import { TransactionRecordService } from '../../services/database/transaction-record-service.database.service.js';
-import { processDiscoveredPaymentTransaction } from '../../services/business-logic/process-discovered-payment-transaction.service.js';
-import { processDiscoveredRefundTransaction } from '../../services/business-logic/process-discovered-refund-transaction.service.js';
+import { PrismaClient, WebsocketSession } from '@prisma/client';
 import { ErrorMessage, ErrorType, errorResponse } from '../../utilities/responses/error-response.utility.js';
 import { PaymentRecordService } from '../../services/database/payment-record-service.database.service.js';
-import { WebsocketSessionService } from '../../services/database/websocket.database.service.js';
 import { sendWebsocketMessage } from '../../services/websocket/send-websocket-message.service.js';
-import { RefundRecordService } from '../../services/database/refund-record-service.database.service.js';
-import { parseAndValidateHeliusHeader } from '../../models/dependencies/helius-header.model.js';
+import { HeliusHeader, parseAndValidateHeliusHeader } from '../../models/dependencies/helius-header.model.js';
 import { processTransaction } from '../../services/business-logic/process-transaction.service.js';
 import axios from 'axios';
 
@@ -28,10 +23,8 @@ Sentry.AWSLambda.init({
 export const helius = Sentry.AWSLambda.wrapHandler(
     async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
         let heliusEnhancedTransactions: HeliusEnhancedTransactionArray;
-        const transactionRecordService = new TransactionRecordService(prisma);
+        let heliusHeaders: HeliusHeader;
         const paymentRecordService = new PaymentRecordService(prisma);
-        const refundRecordService = new RefundRecordService(prisma);
-        const websocketSessionService = new WebsocketSessionService(prisma);
 
         console.log('in helius');
 
@@ -41,20 +34,25 @@ export const helius = Sentry.AWSLambda.wrapHandler(
 
         const requiredAuthorizationHeader = process.env.HELIUS_AUTHORIZATION;
 
-        if (requiredAuthorizationHeader != null) {
-            const authorizationHeader = event.headers['authorization'];
+        if (requiredAuthorizationHeader == null) {
+            return errorResponse(ErrorType.internalServerError, ErrorMessage.missingEnv);
+        }
 
-            if (authorizationHeader !== requiredAuthorizationHeader) {
-                return errorResponse(ErrorType.unauthorized, ErrorMessage.missingHeader);
-            }
+        try {
+            heliusHeaders = parseAndValidateHeliusHeader(event.headers);
+        } catch (error) {
+            Sentry.captureException(error);
+            return errorResponse(ErrorType.badRequest, ErrorMessage.missingHeader);
+        }
+
+        if (heliusHeaders.authorization !== requiredAuthorizationHeader) {
+            return errorResponse(ErrorType.unauthorized, ErrorMessage.missingHeader);
         }
 
         try {
             heliusEnhancedTransactions = parseAndValidateHeliusEnchancedTransaction(JSON.parse(event.body));
         } catch (error) {
-            // Returning an error will get Helius to retry but it might not fix it. We should log as a critical error
             // TODO: Log this, actually might not be critical but we might want to put more logic around seeing if it's critical
-
             Sentry.captureException(error);
             return errorResponse(ErrorType.badRequest, ErrorMessage.invalidRequestBody);
         }
@@ -62,8 +60,6 @@ export const helius = Sentry.AWSLambda.wrapHandler(
         console.log('parsed helius');
 
         for (const heliusTransaction of heliusEnhancedTransactions) {
-            let paymentRecord: PaymentRecord | null = null;
-            let refundRecord: RefundRecord | null = null;
             let websocketSessions: WebsocketSession[] | null = null;
 
             const paymentRecordAndWebsocketService =
