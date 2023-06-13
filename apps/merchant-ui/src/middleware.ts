dfc70a4f96ee22ad7c86d3d664fed320db7bdef7
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-13 19:49:05 -0400

    added middleware file

---
+++ apps/merchant-ui/src/middleware.ts
@@ -0,0 +1,58 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = 'US';
+
+// Trigger this middleware to run on the `/secret-page` route
+// export const config = {
+//     matcher: '/.*',
+// };
+
+const hardBlockCountries = ['CU', 'IR', 'KP', 'RU', 'SY'];
+const hardBlockUkraine = ['crimea', 'donetsk', 'luhansk'];
+
+const isBlockedGeo = (request: NextRequest): boolean => {
+    if (process.env.NODE_ENV === 'development') {
+        return false;
+    }
+
+    const geo = request.geo;
+
+    if (geo == null) {
+        return true;
+    }
+
+    const country = geo.country;
+
+    if (country == null) {
+        return true;
+    }
+
+    if (hardBlockCountries.includes(country)) {
+        return true;
+    }
+
+    return false;
+};
+
+export function middleware(request: NextRequest) {
+    const { nextUrl: url } = request;
+
+    const isBlocked = isBlockedGeo(request);
+    const geo = request.geo;
+
+    if (geo) {
+        url.searchParams.set('country', geo.country ?? 'unknown');
+    }
+
+    console.log('after geo', url);
+
+    if (!geo || (geo && geo.country === BLOCKED_COUNTRY)) {
+        request.nextUrl.pathname = '/';
+    }
+
+    url.searchParams.set('isBlocked', isBlocked.toString());
+
+    return NextResponse.rewrite(url);
+}
