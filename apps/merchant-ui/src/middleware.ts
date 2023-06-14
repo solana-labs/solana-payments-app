ecdefdf84b72e59886a1b9c9ce20a6ae4c9aa7d3
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-14 00:09:18 -0400

    updated matcher

--- apps/merchant-ui/src/middleware.ts
+++ apps/merchant-ui/src/middleware.ts
@@ -9,6 +9,19 @@ const BLOCKED_COUNTRY = 'US';
 //     matcher: '/.*',
 // };
 
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+};
+
 const hardBlockCountries = ['CU', 'IR', 'KP', 'RU', 'SY', 'US'];
 const hardBlockUkraine = ['crimea', 'donetsk', 'luhansk'];
 
