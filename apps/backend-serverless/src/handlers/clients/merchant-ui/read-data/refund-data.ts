02f74f0f47789cb99b019bf66df7035f3c03722c
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-15 18:02:22 -0400

    Cookie Management (#290)
    * updated data handlers and fetchers
    
    * cors and cookie domain adjustment
    
    * updating seeding mechanism to work w default store
    
    * Update merchantStore.ts

--- apps/backend-serverless/src/handlers/clients/merchant-ui/read-data/refund-data.ts
+++ apps/backend-serverless/src/handlers/clients/merchant-ui/read-data/refund-data.ts
@@ -1,16 +1,16 @@
+import { Merchant, PrismaClient } from '@prisma/client';
 import * as Sentry from '@sentry/serverless';
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { Merchant, PrismaClient } from '@prisma/client';
-import { MerchantService } from '../../../../services/database/merchant-service.database.service.js';
 import { MerchantAuthToken } from '../../../../models/clients/merchant-ui/merchant-auth-token.model.js';
 import {
     RefundDataRequestParameters,
     parseAndValidateRefundDataRequestParameters,
 } from '../../../../models/clients/merchant-ui/refund-data-request.model.js';
-import { withAuth } from '../../../../utilities/clients/merchant-ui/token-authenticate.utility.js';
-import { Pagination } from '../../../../utilities/clients/merchant-ui/database-services.utility.js';
+import { MerchantService } from '../../../../services/database/merchant-service.database.service.js';
 import { createGeneralResponse } from '../../../../utilities/clients/merchant-ui/create-general-response.js';
 import { createRefundResponse } from '../../../../utilities/clients/merchant-ui/create-refund-response.utility.js';
+import { Pagination } from '../../../../utilities/clients/merchant-ui/database-services.utility.js';
+import { withAuth } from '../../../../utilities/clients/merchant-ui/token-authenticate.utility.js';
 import { ErrorMessage, ErrorType, errorResponse } from '../../../../utilities/responses/error-response.utility.js';
 
 const prisma = new PrismaClient();
@@ -74,6 +74,10 @@ export const refundData = Sentry.AWSLambda.wrapHandler(
         return {
             statusCode: 200,
             body: JSON.stringify(responseBodyData),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
         };
     },
     {
