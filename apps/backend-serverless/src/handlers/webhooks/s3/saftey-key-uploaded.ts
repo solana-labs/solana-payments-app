fecb71c61d1b4e3e90cf16f7ae61f3d4b5d0c1e0
littlejohntj <tjl13c@my.fsu.edu>
2023-06-19 17:05:18 -0400

    deleting single use on successful tx

--- apps/backend-serverless/src/handlers/webhooks/s3/saftey-key-uploaded.ts
+++ apps/backend-serverless/src/handlers/webhooks/s3/saftey-key-uploaded.ts
@@ -5,16 +5,6 @@ import { startExecutionOfSafteySweep } from '../../../services/step-function/sta
 
 export const safteyKeyUploaded = Sentry.AWSLambda.wrapHandler(
     async (event: S3Event): Promise<APIGatewayProxyResultV2> => {
-        // try {
-        //     shopifyMutationRetry = parseAndValidateShopifyMutationRetry(event);
-        // } catch (error) {
-        //     // Throwing should cause the step function to be retried. However, we should
-        //     // manually view the error becuase it shouldn't work next titme either.
-        //     // TODO: Log more data with the capture because the data here should be good to parse
-        //     Sentry.captureException(error);
-        //     throw new InvalidInputError('shopify mutation retry body'); // TODO: Critical Error
-        // }
-
         for (const record of event.Records) {
             try {
                 await startExecutionOfSafteySweep(record.s3.object.key);
@@ -23,8 +13,6 @@ export const safteyKeyUploaded = Sentry.AWSLambda.wrapHandler(
                 throw new Error('Could not execute the shopify mutation step function');
             }
         }
-
-        console.log('made it to saftey key uploaded');
 
         return {
             statusCode: 200,
