2471e5e5e4b57ba8334e6a20ffda61fcd791b3cb
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-20 16:41:18 -0400

    Fixed merchant address update text

--- apps/merchant-ui/src/components/MerchantInfo.tsx
+++ apps/merchant-ui/src/components/MerchantInfo.tsx
@@ -69,8 +69,9 @@ export function MerchantInfo(props: Props) {
         if (response && response.status === 200) {
             setAddressChanged(true);
         } else if (response && response.status !== 200) {
-            console.log('in not changed', response);
-            setAddressChanged(response?.statusText);
+            let d = await response.json();
+            console.log('in not changed', d.error);
+            setAddressChanged(d.error);
         }
         await getMerchantInfo();
         setPending(false);
