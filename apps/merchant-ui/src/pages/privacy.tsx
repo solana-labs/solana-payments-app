b61b3ef2115925da975ae248e01fc1082a976c19
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-21 08:50:09 -0400

    added page formatting

--- apps/merchant-ui/src/pages/privacy.tsx
+++ apps/merchant-ui/src/pages/privacy.tsx
@@ -1,17 +1,36 @@
 import { DefaultLayout } from '@/components/DefaultLayout';
-import { PrivacyPolicyText } from '@/lib/policies';
+import { DefaultLayoutContent } from '@/components/DefaultLayoutContent';
+import { DefaultLayoutScreenTitle } from '@/components/DefaultLayoutScreenTitle';
+import { privacyPolicySections } from '@/lib/policies';
 import Head from 'next/head';
+import React from 'react';
+
+const title = 'Privacy Policy';
 
 export default function Privacy() {
     return (
         <>
             <Head>
-                <title>Solana Pay - Privacy Policy</title>
-                <meta name="description" content="Privacy Policy" />
+                <title>Solana Pay - {title}</title>
+                <meta name="description" content={title} />
             </Head>
             <div className="h-screen w-screen">
                 <DefaultLayout className="h-full w-full">
-                    <PrivacyPolicyText />
+                    <DefaultLayoutContent>
+                        <DefaultLayoutScreenTitle>{title}</DefaultLayoutScreenTitle>
+                        <div className="text-justify bg-white rounded-lg text-sm leading-6 mt-16">
+                            {privacyPolicySections.map((section, index) => (
+                                <React.Fragment key={index}>
+                                    <h3 className="font-semibold text-2xl">{section.title}</h3>
+                                    {section.paragraphs.map((paragraph, i) => (
+                                        <p className={i === 0 ? '' : 'mt-4'} key={i}>
+                                            {paragraph}
+                                        </p>
+                                    ))}
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    </DefaultLayoutContent>
                 </DefaultLayout>
             </div>
         </>
--- apps/merchant-ui/src/pages/termsofservice.tsx
+++ apps/merchant-ui/src/pages/termsofservice.tsx
@@ -1,17 +1,36 @@
 import { DefaultLayout } from '@/components/DefaultLayout';
-import { TosText } from '@/lib/policies';
+import { DefaultLayoutContent } from '@/components/DefaultLayoutContent';
+import { DefaultLayoutScreenTitle } from '@/components/DefaultLayoutScreenTitle';
+import { tosSections } from '@/lib/policies';
 import Head from 'next/head';
+import React from 'react';
+
+const title = 'Terms of Service';
 
 export default function TermsOfService() {
     return (
         <>
             <Head>
-                <title>Solana Pay - Terms of Service</title>
-                <meta name="description" content="Terms of Service" />
+                <title>Solana Pay - {title}</title>
+                <meta name="description" content={title} />
             </Head>
             <div className="h-screen w-screen">
                 <DefaultLayout className="h-full w-full">
-                    <TosText />
+                    <DefaultLayoutContent>
+                        <DefaultLayoutScreenTitle>{title}</DefaultLayoutScreenTitle>
+                        <div className="text-justify bg-white rounded-lg text-sm leading-6 mt-16">
+                            {tosSections.map((section, index) => (
+                                <React.Fragment key={index}>
+                                    <h3 className="font-semibold text-2xl">{section.title}</h3>
+                                    {section.paragraphs.map((paragraph, i) => (
+                                        <p className={i === 0 ? '' : 'mt-4'} key={i}>
+                                            {paragraph}
+                                        </p>
+                                    ))}
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    </DefaultLayoutContent>
                 </DefaultLayout>
             </div>
         </>
