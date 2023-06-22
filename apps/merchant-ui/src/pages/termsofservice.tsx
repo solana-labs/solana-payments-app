2ecdf3a4d61123a71db524047c4b486f3e8e12ac
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-06-22 16:46:29 -0400

    updated privacy and tos pages to show pdfs

--- apps/merchant-ui/src/pages/termsofservice.tsx
+++ apps/merchant-ui/src/pages/termsofservice.tsx
@@ -1,9 +1,8 @@
 import { DefaultLayout } from '@/components/DefaultLayout';
 import { DefaultLayoutContent } from '@/components/DefaultLayoutContent';
 import { DefaultLayoutScreenTitle } from '@/components/DefaultLayoutScreenTitle';
-import { tosSections } from '@/lib/policies';
+import { PdfViewer } from '@/components/PdfViewer';
 import Head from 'next/head';
-import React from 'react';
 
 const title = 'Terms of Service';
 
@@ -18,18 +17,7 @@ export default function TermsOfService() {
                 <DefaultLayout className="h-full w-full">
                     <DefaultLayoutContent>
                         <DefaultLayoutScreenTitle>{title}</DefaultLayoutScreenTitle>
-                        <div className="text-justify bg-white rounded-lg text-sm leading-6 mt-16">
-                            {tosSections.map((section, index) => (
-                                <React.Fragment key={index}>
-                                    <h3 className="font-semibold text-2xl">{section.title}</h3>
-                                    {section.paragraphs.map((paragraph, i) => (
-                                        <p className={i === 0 ? '' : 'mt-4'} key={i}>
-                                            {paragraph}
-                                        </p>
-                                    ))}
-                                </React.Fragment>
-                            ))}
-                        </div>
+                        <PdfViewer title={'Terms of Service'} />
                     </DefaultLayoutContent>
                 </DefaultLayout>
             </div>
