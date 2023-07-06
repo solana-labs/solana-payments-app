5feb98736772c2b85529c857cd3d1cc522759613
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-06 02:30:48 -0400

    proper parsing of shop url

--- apps/backend-serverless/src/models/shopify/install-query-params.model.ts
+++ apps/backend-serverless/src/models/shopify/install-query-params.model.ts
@@ -1,9 +1,33 @@
-import { object, string, InferType } from 'yup';
+import { InferType, object, string } from 'yup';
 import { parseAndValidate } from '../../utilities/yup.utility.js';
+
+// export const appInstallQueryParmSchema = object().shape({
+//     hmac: string().optional(),
+//     shop: string()
+//         .test('shop', 'Invalid shop', value =>
+//             process.env.NODE_ENV === 'development'
+//                 ? (value && /[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.com\/?/.test(value)) || value === 'localhost:4004'
+//                 : /[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.com\/?/.test(value)
+//         )
+//         .required(),
+//     host: string().required(),
+//     timestamp: string().required(),
+// });
+
 export const appInstallQueryParmSchema = object().shape({
     hmac: string().optional(), // optional for the purpose of validation. i should do an extra check here or make a new structure
     shop: string()
-        .matches(/[a-zA-Z0-9][a-zA-Z0-9\-]*\.myshopify\.com\/?/)
+        .test('shop', 'Invalid shop', (value: string | undefined) => {
+            if (typeof value === 'undefined') {
+                return false;
+            } else if (process.env.NODE_ENV === 'development') {
+                console.log('in devleopment node env shop');
+                return value === 'localhost:4004';
+            } else {
+                console.log('in not development node env shop');
+                return /[a-zA-Z0-9][a-zA-Z0-9-]*\.myshopify\.com\/?/.test(value);
+            }
+        })
         .required(),
     host: string().required(),
     timestamp: string().required(),
