97915ade9b0ccef0106451b75bc385ed5919d166
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-13 05:28:16 -0400

    error handling fix hmac works!

--- apps/backend-serverless/src/handlers/webhooks/gdpr/shop-redact.ts
+++ apps/backend-serverless/src/handlers/webhooks/gdpr/shop-redact.ts
@@ -38,8 +38,6 @@ export const shopRedact = Sentry.AWSLambda.wrapHandler(
             },
         });
 
-        console.log('in shop redact', event);
-
         const merchantService = new MerchantService(prisma);
         const gdprService = new GDPRService(prisma);
 
@@ -54,32 +52,15 @@ export const shopRedact = Sentry.AWSLambda.wrapHandler(
             return createErrorResponse(new InvalidInputError('incorrect topic for shop redact'));
         }
 
-        Sentry.captureEvent({
-            message: 'In shopRedact gdpr correct topic',
-            level: 'info',
-        });
-
         if (event.body == null) {
             return createErrorResponse(new InvalidInputError('Shop redact Missing body'));
         }
-
-        Sentry.captureEvent({
-            message: 'In shopRedact gdpr valid body',
-            level: 'info',
-        });
-
-        const shopRedactBodyString = JSON.stringify(event.body);
 
         try {
             verifyShopifyWebhook(Buffer.from(event.body), webhookHeaders['x-shopify-hmac-sha256']);
         } catch (error) {
             return createErrorResponse(error);
         }
-
-        Sentry.captureEvent({
-            message: 'valid hmac',
-            level: 'info',
-        });
 
         let shopRedactRequest: ShopRedactRequest;
 
@@ -89,38 +70,17 @@ export const shopRedact = Sentry.AWSLambda.wrapHandler(
             return createErrorResponse(error);
         }
 
-        Sentry.captureEvent({
-            message: 'In shopRedact gdpr parsed body',
-            level: 'info',
-            extra: {
-                shopRedactRequest: JSON.stringify(shopRedactRequest),
-            },
-        });
-
         const merchant = await merchantService.getMerchant({ shop: shopRedactRequest.shop_domain });
 
         if (merchant == null) {
             return createErrorResponse(new MissingExpectedDatabaseRecordError('merchant'));
         }
 
-        Sentry.captureEvent({
-            message: 'In shopRedact got merchant',
-            level: 'info',
-            extra: {
-                merchant: JSON.stringify(merchant),
-            },
-        });
-
         try {
             await gdprService.createGDPRRequest(merchant.id);
         } catch (error) {
             return createErrorResponse(error);
         }
-
-        Sentry.captureEvent({
-            message: 'In shopRedact sent gdpr, we guuci',
-            level: 'info',
-        });
 
         return {
             statusCode: 200,
