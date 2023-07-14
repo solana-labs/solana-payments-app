f2e68b45e7dd65be0ea30424e99152eaa1b5457b
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-14 09:55:48 -0400

    removed typescript and more boilerplate

--- docs/docs/docusaurus.config.js
+++ docs/docs/docusaurus.config.js
@@ -1,37 +1,17 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'Shopify <> Solana Pay',
     tagline: 'Enable Solana powered Payments on Shopify',
     favicon: 'img/favicon.ico',
-
-    // Set the production url of your site here
     url: 'https://shopifydocs.solanapay.com',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
-
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'solanalabs', // Usually your GitHub org/user name.
-    projectName: 'solana-payments-app', // Usually your repo name.
-
+    organizationName: 'solana-labs',
+    projectName: 'solana-payments-app',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
-
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'en',
         locales: ['en'],
     },
-
     presets: [
         [
             '@docusaurus/preset-classic',
@@ -48,82 +28,27 @@ const config = {
         ],
     ],
 
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            // Replace with your project's social card
-            image: 'img/docusaurus-social-card.jpg',
-            navbar: {
-                title: 'Solana Payments App',
-                logo: {
-                    alt: 'My Site Logo',
-                    src: 'img/logo.png',
+    themeConfig: {
+        navbar: {
+            title: 'Solana Payments App',
+            logo: {
+                alt: 'My Site Logo',
+                src: 'img/logo.png',
+            },
+            items: [
+                {
+                    href: 'https://github.com/solana-labs/solana-payments-app',
+                    label: 'GitHub',
+                    position: 'right',
                 },
-                items: [
-                    {
-                        type: 'docSidebar',
-                        sidebarId: 'tutorialSidebar',
-                        position: 'left',
-                        label: 'Documentation',
-                    },
-
-                    {
-                        href: 'https://github.com/solana-labs/solana-payments-app',
-                        label: 'GitHub',
-                        position: 'right',
-                    },
-                ],
-            },
-            footer: {
-                style: 'dark',
-                links: [
-                    {
-                        title: 'Docs',
-                        items: [
-                            {
-                                label: 'Documentation',
-                                to: '/',
-                            },
-                        ],
-                    },
-                    // {
-                    //     title: 'Community',
-                    //     items: [
-                    //         {
-                    //             label: 'Stack Overflow',
-                    //             href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                    //         },
-                    //         {
-                    //             label: 'Discord',
-                    //             href: 'https://discordapp.com/invite/docusaurus',
-                    //         },
-                    //         {
-                    //             label: 'Twitter',
-                    //             href: 'https://twitter.com/docusaurus',
-                    //         },
-                    //     ],
-                    // },
-                    {
-                        title: 'More',
-                        items: [
-                            // {
-                            //     label: 'Blog',
-                            //     to: '/blog',
-                            // },
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/facebook/docusaurus',
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-        }),
+            ],
+        },
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: false,
+            respectPrefersColorScheme: false,
+        },
+    },
 };
 
 module.exports = config;
