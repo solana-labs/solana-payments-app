89ed2f4e77f861a76f40666270c9765fc09c05ec
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-15 17:28:55 -0400

    some formatting bugs

--- apps/payment-ui/src/components/ThankYou.tsx
+++ apps/payment-ui/src/components/ThankYou.tsx
@@ -1,15 +1,23 @@
-import Image from 'next/image'
-import FinishHandler from './handlers/FinishHandler'
+import Image from 'next/image';
+import FinishHandler from './handlers/FinishHandler';
 
 export const ThankYouView = () => {
     return (
         <div className="flex flex-col">
             <FinishHandler />
-            <Image src="/check.svg" alt="Completed Payment" className="mx-auto w-16 h-16 mt-7 mb-4" width={10} height={10} />
+            <Image
+                src="/check.svg"
+                alt="Completed Payment"
+                className="mx-auto w-16 h-16 mt-7 mb-4"
+                width={10}
+                height={10}
+            />
             <div className="text-3xl text-black mx-auto">Thanks for your order.</div>
             <div className="text-1xl text-gray-600 mx-auto pt-4">Your payment was successful.</div>
             <div className="divider w-full" />
-            <div className="text-sm text-gray-400 font-light mx-auto">We're redirecting you back to Shopify...</div>
+            <div className="text-sm text-gray-400 font-light mx-auto">
+                We&apos;re redirecting you back to Shopify...
+            </div>
         </div>
-    )
-}
+    );
+};
--- apps/payment-ui/src/components/WalletCopyButton.tsx
+++ apps/payment-ui/src/components/WalletCopyButton.tsx
@@ -1,26 +1,22 @@
 import { useWallet } from '@solana/wallet-adapter-react';
-import { FC, MouseEventHandler, useState } from 'react';
-import React, { useCallback, useMemo } from 'react';
-import type { ButtonProps } from './Button';
-import { Button } from './Button';
-import { WalletIcon } from '@solana/wallet-adapter-react-ui';
 import Image from 'next/image';
+import { FC, MouseEventHandler, useCallback, useMemo, useState } from 'react';
+import type { ButtonProps } from './Button';
 
 export const WalletCopyButton: FC<ButtonProps> = ({ children, disabled, onClick, ...props }) => {
     const { publicKey, wallet, disconnect } = useWallet();
     const [copied, setCopied] = useState(false);
 
     const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
-        (event) => {
+        event => {
             if (onClick) onClick(event);
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            // eslint-disable-next-line
             if (!event.defaultPrevented) disconnect().catch(() => {});
         },
-        [onClick, disconnect]
+        [onClick, disconnect],
     );
 
     const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
-
 
     const copyAddress = useCallback(async () => {
         if (base58) {
@@ -31,16 +27,17 @@ export const WalletCopyButton: FC<ButtonProps> = ({ children, disabled, onClick,
     }, [base58]);
 
     return (
-        <div className='flex flex-row justify-center'>
-            <button className='btn w-full outline-none border-2 border-black bg-white hover:bg-white text-black flex justify-center items-center normal-case' onClick={copyAddress}>
-                <div className='flex flex-row items-center justify-center'>
+        <div className="flex flex-row justify-center">
+            <button
+                className="btn w-full outline-none border-2 border-black bg-white hover:bg-white text-black flex justify-center items-center normal-case"
+                onClick={copyAddress}
+            >
+                <div className="flex flex-row items-center justify-center">
                     <Image src="/content_copy.svg" alt="Solana Pay Logo" width={18} height={18} />
-                    <div className='pr-1'></div>
-                    <div className='pl-1 text-lg'>{copied ? 'Copied' : 'Copy address'}</div>
+                    <div className="pr-1"></div>
+                    <div className="pl-1 text-lg">{copied ? 'Copied' : 'Copy address'}</div>
                 </div>
             </button>
         </div>
     );
-        
-        
 };
