9c0d520c9f3da8c1417144b0206713aa5ca7897b
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-16 00:00:28 -0400

    Eslint Configs (#423)
    * eslint and prettier working lfg
    
    * solana prettier and formatting
    
    * preitter clean
    
    * some more small changes

--- apps/transaction-request-serverless/index.js
+++ apps/transaction-request-serverless/index.js
@@ -1,13 +1,13 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.handler = async event => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Go Serverless v3.0! Your function executed successfully!',
+                input: event,
+            },
+            null,
+            2,
+        ),
+    };
 };
