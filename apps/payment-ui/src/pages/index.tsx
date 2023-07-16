1d5b549f26744ffec5aee59e713f99c136facb28
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-16 19:56:23 -0400

    final cleans

--- apps/payment-ui/src/pages/index.tsx
+++ apps/payment-ui/src/pages/index.tsx
@@ -1,10 +1,6 @@
 import FooterSection from '@/components/FooterSection';
-import { getPaymentMethod } from '@/features/payment-options/paymentOptionsSlice';
-import { getMergedState } from '@/features/payment-session/paymentSessionSlice';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import Head from 'next/head';
 import React from 'react';
-import { useSelector } from 'react-redux';
 import DisplaySection from '../components/DisplaySection';
 import MainSection from '../components/MainSection';
 
@@ -16,22 +12,9 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
     };
 };
 
-export type BlockedProps = {
-    isBlocked: string;
-    country: string;
-};
-
-export default function Home({ isBlocked, country }: BlockedProps) {
-    const paymentMethod = useSelector(getPaymentMethod);
-    const mergedState = useSelector(getMergedState);
-
+export default function Home() {
     return (
         <>
-            <Head>
-                <title>Solana Pay Payment Portal</title>
-                <meta name="description" content="Use Solana Pay for your Shopify Checkout" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
             <React.Fragment>
                 <div className="flex flex-col h-screen bg-black">
                     <DisplaySection />
