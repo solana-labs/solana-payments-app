8f7172ab27198fdd349d26a6b54978f869b65e79
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-16 18:29:53 -0400

    error view unify

--- apps/payment-ui/src/components/GeoBlockedView.tsx
+++ apps/payment-ui/src/components/GeoBlockedView.tsx
@@ -1,9 +1,15 @@
 import { getPaymentDetails } from '@/features/payment-details/paymentDetailsSlice';
 import { useSelector } from 'react-redux';
-import { ErrorGoBack } from './ErrorGoBack';
+import { ErrorView } from './ErrorView';
 
 export const GeoBlockedView = () => {
     const paymentDetails = useSelector(getPaymentDetails);
 
-    return <ErrorGoBack top={'Blocked'} bottom={'Nice try kid.'} redirect={paymentDetails?.cancelUrl ?? null} />;
+    const error = {
+        top: 'GeoBlocked',
+        bottom: 'Sorry, we are not allowed to operate in your jurisdiction',
+        redirect: paymentDetails?.cancelUrl ?? null,
+    };
+
+    return <ErrorView error={error} />;
 };
