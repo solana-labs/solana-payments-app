dab69e702e095dde12849e9f3c398398d1c50c69
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-16 18:44:35 -0400

    comment

--- apps/payment-ui/src/components/BuyButton.tsx
+++ apps/payment-ui/src/components/BuyButton.tsx
@@ -116,19 +116,4 @@ const BuyButton = () => {
     );
 };
 
-//     return (
-//         <button
-//             disabled={isDisabled()}
-//             onClick={async () => {
-//                 await fetchAndSendTransaction();
-//             }}
-//             className="btn w-full bg-black text-white py-4 pt-3 text-base rounded-md shadow-lg disabled:shadow-none font-semibold flex justify-center items-center normal-case disabled:bg-slate-200 disabled:text-slate-400"
-//         >
-//             <div className={`flex flex-row items-center justify-center`}>
-//                 {isLoading ? <span className="loading loading-spinner loading-sm mr-1" /> : <div />}
-//                 <div className="ml-1">Buy now</div>
-//             </div>
-//         </button>
-//     );
-
 export default BuyButton;
