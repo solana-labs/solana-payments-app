c601cdc0f711c41a92f0458ed11995d2f3faefe6
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-16 21:26:13 -0400

    shop page added

--- apps/payment-ui/src/pages/[shop]/index.tsx
+++ apps/payment-ui/src/pages/[shop]/index.tsx
@@ -1,19 +1,37 @@
-import CheckoutSection from '@/components/CheckoutSection';
 import { DefaultLayout } from '@/components/DefaultLayout';
-import FooterSection from '@/components/FooterSection';
 import { useRouter } from 'next/router';
 
-export default function CheckoutPage() {
+export default function ShopPage() {
     const router = useRouter();
+    const { shop } = router.query;
 
-    if (!router.isReady) {
-        return <div>Loading...</div>;
-    }
+    // Mocked user data
+    const userData = {
+        points: 200,
+        nfts: [
+            { name: 'NFT 1', imageUrl: '/path/to/image1.png' },
+            { name: 'NFT 2', imageUrl: '/path/to/image2.png' },
+        ],
+        tiers: 'Gold',
+    };
 
     return (
         <DefaultLayout>
-            <CheckoutSection />
-            <FooterSection />
+            <h1 className="text-4xl font-bold mb-4">{`Welcome to your rewards for ${shop}`}</h1>
+            <div className="flex flex-col">
+                <h2 className="text-2xl font-bold mb-2">Your rewards:</h2>
+                <p className="text-lg mb-2">{`Points: ${userData.points}`}</p>
+                <p className="text-lg mb-2">{`Tier: ${userData.tiers}`}</p>
+                <div>
+                    <h3 className="text-lg font-bold mb-2">Your NFTs:</h3>
+                    {userData.nfts.map((nft, index) => (
+                        <div key={index} className="mb-2">
+                            {/* <img src={nft.imageUrl} alt={nft.name} className="mb-1" /> */}
+                            <p className="text-lg">{nft.name}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </DefaultLayout>
     );
 }
