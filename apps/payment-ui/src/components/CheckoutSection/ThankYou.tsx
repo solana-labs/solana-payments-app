9819fadfab98cb893fdbf098ebfd7036edb923b1
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-16 23:25:04 -0400

    reorged checkout views

--- apps/payment-ui/src/components/CheckoutSection/ThankYou.tsx
+++ apps/payment-ui/src/components/CheckoutSection/ThankYou.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import FinishHandler from './handlers/FinishHandler';
+import FinishHandler from '../handlers/FinishHandler';
 
 export const ThankYouView = () => {
     return (
