8ce0a37f13365a9606cad17d0075e6ba377de738
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-07-31 14:14:37 -0400

    fetching product images on product webhook (#494)

--- apps/backend-serverless/src/utilities/clients/create-loyalty-response.utility.ts
+++ apps/backend-serverless/src/utilities/clients/create-loyalty-response.utility.ts
@@ -1,6 +1,5 @@
 import { LoyaltyProgram, Merchant, PrismaClient, Product, Tier } from '@prisma/client';
 import { MerchantService } from '../../services/database/merchant-service.database.service.js';
-import { fetchAllProducts } from '../../services/shopify/shop-products.service.js';
 
 export interface LoyaltyResponse {
     loyaltyProgram: LoyaltyProgram;
@@ -14,24 +13,8 @@ export interface LoyaltyResponse {
 
 const prisma = new PrismaClient();
 
-function shouldUpdateProducts(lastFetched) {
-    if (lastFetched == null) {
-        return true;
-    } else {
-        const now = new Date();
-        const twoMinutesLater = new Date(lastFetched.getTime() + 2 * 60000);
-        return now > twoMinutesLater;
-    }
-}
-
 export const createLoyaltyResponse = async (merchant: Merchant): Promise<LoyaltyResponse> => {
     const merchantService = new MerchantService(prisma);
-
-    if (shouldUpdateProducts(merchant.lastFetched)) {
-        const fetchedProducts = await fetchAllProducts(merchant);
-        await merchantService.upsertProducts(merchant.id, fetchedProducts);
-        await merchantService.updateMerchant(merchant, { lastFetched: new Date() });
-    }
 
     const products = await merchantService.getProductsByMerchant(merchant.id);
     const tiers = await merchantService.getTiers(merchant.id);
