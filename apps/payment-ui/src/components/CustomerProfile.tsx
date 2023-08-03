2c65f1e845b78de88858d014e928cfb01414fbd3
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-08-03 16:48:54 -0400

    show customer tier in profile

--- apps/payment-ui/src/components/CustomerProfile.tsx
+++ apps/payment-ui/src/components/CustomerProfile.tsx
@@ -8,7 +8,7 @@ import {
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog';
-import { getBalance, getCustomerNfts, getPointsBalance } from '@/features/customer/customerSlice';
+import { getBalance, getCustomerNfts, getPointsBalance, getTier } from '@/features/customer/customerSlice';
 import { useWallet } from '@solana/wallet-adapter-react';
 import * as web3 from '@solana/web3.js';
 import Image from 'next/image';
@@ -25,6 +25,7 @@ export function CustomerProfile(props: Props) {
 
     const usdcBalance = useSelector(getBalance);
     const pointsBalance = useSelector(getPointsBalance);
+    const customerTier = useSelector(getTier);
     const customerNfts = useSelector(getCustomerNfts);
     const [copied, setCopied] = useState(false);
 
@@ -68,6 +69,11 @@ export function CustomerProfile(props: Props) {
                         <DialogDescription className="pt-2 text-gray-700 text-md font-normal">
                             {pointsBalance} Points
                         </DialogDescription>
+                        {customerTier && (
+                            <DialogDescription className="pt-2 text-gray-700 text-md font-normal">
+                                {customerTier.name}
+                            </DialogDescription>
+                        )}
                         {customerNfts && (
                             <DialogDescription className="pt-2 text-gray-700 text-md font-normal text-center">
                                 <div>
