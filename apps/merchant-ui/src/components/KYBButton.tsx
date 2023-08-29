c0e1b210b37d85e00fbde82ff89aecbf3b815c50
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-08-28 21:03:20 -0400

    Updated KYB states

--- apps/merchant-ui/src/components/KYBButton.tsx
+++ apps/merchant-ui/src/components/KYBButton.tsx
@@ -29,57 +29,60 @@ export function KYBButton(props: Props) {
 
     return kybState === 'finished' ? (
         <div className="flex items-center">
-            <div className="text-emerald-700 text-sm font-semibold mr-2">Approved</div>
+            <div className="text-emerald-700 text-sm font-semibold">Approved</div>
             <CheckmarkCircle className="h-5 fill-emerald-700 w-5" />
         </div>
     ) : kybState === 'pending' ? (
         <div className="flex items-center bg-indigo-100 px-4 py-2.5 rounded-lg">
-            <div className="text-indigo-700 text-sm font-semibold mr-2">Pending email verification</div>
+            <div className="text-indigo-700 text-sm font-semibold">Pending review</div>
         </div>
-    ) : kybState === 'failed' ? (
-        <div className="flex items-center bg-red-100 px-4 py-2.5 rounded-lg">
-            <div className="text-red-700 text-sm font-semibold mr-2">Verification failed</div>
-        </div>
-    ) : kybState === 'incomplete' ? (
-        <Dialog.Root open={open} onOpenChange={setOpen}>
-            <Dialog.Trigger asChild>
-                <Primary className={props.className}>Start</Primary>
-            </Dialog.Trigger>
-            <Dialog.Portal>
-                <Dialog.Overlay
-                    className={twMerge(
-                        'bg-black/30',
-                        'bottom-0',
-                        'fixed',
-                        'grid',
-                        'left-0',
-                        'place-items-center',
-                        'right-0',
-                        'top-0',
-                        'z-10'
-                    )}
-                >
-                    <Dialog.Content
-                        className="bg-white rounded-xl overflow-hidden"
-                        style={{ width: PERSONA_WIDTH, height: PERSONA_HEIGHT }}
+    ) : kybState === 'incomplete' || kybState === 'failed' ? (
+        <>
+            {kybState === 'failed' && (
+                <div className="flex items-center bg-red-100 px-4 py-2.5 rounded-lg">
+                    <div className="text-red-700 text-sm font-semibold">Verification failed</div>
+                </div>
+            )}
+            <Dialog.Root open={open} onOpenChange={setOpen}>
+                <Dialog.Trigger asChild>
+                    <Primary className={props.className}>{kybState === 'incomplete' ? 'Start' : 'Restart'}</Primary>
+                </Dialog.Trigger>
+                <Dialog.Portal>
+                    <Dialog.Overlay
+                        className={twMerge(
+                            'bg-black/30',
+                            'bottom-0',
+                            'fixed',
+                            'grid',
+                            'left-0',
+                            'place-items-center',
+                            'right-0',
+                            'top-0',
+                            'z-10'
+                        )}
                     >
-                        <Inquiry
-                            templateId="itmpl_r9DWaWkBDNJb2KTd1c83i5Xg"
-                            referenceId={RE.isOk(merchantInfo) ? merchantInfo.data.shop : ''}
-                            environmentId={process.env.NEXT_PUBLIC_PERSONA_ENVIRONMENT_ID}
-                            frameWidth={PERSONA_WIDTH}
-                            frameHeight={PERSONA_HEIGHT}
-                            onComplete={async ({ inquiryId, status, fields }) => {
-                                await updateMerchant('kybInquiry', inquiryId);
-                                await getMerchantInfo();
-                                props.onVerified?.();
-                                setOpen(false);
-                            }}
-                        />
-                    </Dialog.Content>
-                </Dialog.Overlay>
-            </Dialog.Portal>
-        </Dialog.Root>
+                        <Dialog.Content
+                            className="bg-white rounded-xl overflow-hidden"
+                            style={{ width: PERSONA_WIDTH, height: PERSONA_HEIGHT }}
+                        >
+                            <Inquiry
+                                templateId="itmpl_r9DWaWkBDNJb2KTd1c83i5Xg"
+                                referenceId={RE.isOk(merchantInfo) ? merchantInfo.data.shop : ''}
+                                environmentId={process.env.NEXT_PUBLIC_PERSONA_ENVIRONMENT_ID}
+                                frameWidth={PERSONA_WIDTH}
+                                frameHeight={PERSONA_HEIGHT}
+                                onComplete={async ({ inquiryId, status, fields }) => {
+                                    await updateMerchant('kybInquiry', inquiryId);
+                                    await getMerchantInfo();
+                                    props.onVerified?.();
+                                    setOpen(false);
+                                }}
+                            />
+                        </Dialog.Content>
+                    </Dialog.Overlay>
+                </Dialog.Portal>
+            </Dialog.Root>
+        </>
     ) : (
         <LoadingDots />
     );
